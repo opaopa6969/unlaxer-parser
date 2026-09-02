b87f4621beb0ab0e04a1e342533674bc34bc4629
opaopa6969 <opaopa6969@gmail.com>
2026-09-02 17:55:30 +0900

    fix: reject character 128 in mapped parser

--- unlaxer-common/src/test/java/org/unlaxer/elementary/MappedSingleCharacterParserTest.java
+++ unlaxer-common/src/test/java/org/unlaxer/elementary/MappedSingleCharacterParserTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.unlaxer.Parsed;
 import org.unlaxer.ParserTestBase;
+import org.unlaxer.Range;
 import org.unlaxer.parser.combinator.OneOrMore;
 import org.unlaxer.parser.elementary.MappedSingleCharacterParser;
 import org.unlaxer.parser.posix.PunctuationParser;
@@ -55,6 +56,16 @@ public class MappedSingleCharacterParserTest extends ParserTestBase {
 		MappedSingleCharacterParser onlyA = new MappedSingleCharacterParser(false, "a");
 		Parsed parsed = parse(onlyA, "𝄞");
 		assertFalse(parsed.isSucceeded());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void rejectsCharacter128() {
+		new MappedSingleCharacterParser((char) 128);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void rejectsRangeEndingAt128() {
+		new MappedSingleCharacterParser(new Range(0, 128));
 	}
 
 }
